1374897a39cceca17fbbd521adcad8b9aad5919d
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-05-18 15:08:32 -0500

    first commt

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import {Provider} from "react-redux";
+import store from "./store";
 
+import "whatwg-fetch";
+
+console.log("Execution test");
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
   document.getElementById("root")
 );
