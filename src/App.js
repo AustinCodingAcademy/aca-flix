9072aac146bb4ef4123c547b2c12ab0f7c3fa245
RX7RUSS <russ.savage5@gmail.com>
2017-05-21 13:03:42 -0500

    state and basic funcitonal components

--- src/App.js
+++ src/App.js
@@ -13,35 +13,19 @@ class App extends Component {
         <header className="Header">
           <Logo />
           {/*  <Navigation>   */}
-          <div id="navigation" className="Navigation">
-            <nav>
-              <ul>
-                <li>Browse</li>
-                <li>My list</li>
-                <li>Top picks</li>
-                <li>Recent</li>
-              </ul>
-            </nav>
-          </div>
+
           {/*  </Navigation>   */}
           <SearchBox />
           {/*  <UserProfile>   */}
-          <div className="UserProfile">
-            <div className="User">
-              <div className="name">Jack Oliver</div>
-              <div className="image">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
-              </div>
-            </div>
-          </div>
+          
           {/*  </UserProfile>   */}
         </header>
         <Hero />
-        <TitleList 
-          title="Search Results" 
+        <TitleList
+          title="Search Results"
           movies={this.props.searchResults} />
-        <TitleList 
-          title="My Movies" 
+        <TitleList
+          title="My Movies"
           movies={this.props.myMovieList} />
       </div>
     );
