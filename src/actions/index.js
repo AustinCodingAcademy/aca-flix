e4f8c19f9869c3323484b48e4b386465dd7896d2
stuartkilgore <stuart.kilgore@gmail.com>
2017-08-21 20:56:55 -0500

    stuff

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+const apiKey = "?api_key=b130452da9fef70fea572ae841e97864";
+
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+
+function searchResults(state = [], action) {
+  return state;
+}
+
