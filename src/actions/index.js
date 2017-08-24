273beb4eb7090a248b7491ca15c2d1cc3a198cc3
stackbaxter <stackbaxter@yahoo.com>
2017-08-24 02:06:51 -0500

    ContainerTime!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,48 +1,49 @@
-export function loadMyMovieList() {
-  return function(dispatch){
-
-    type: "LOAD_MY_MOVIE_LIST"
-      }
-    
+export function loadMyMoviesList() {
+  return function (dispatch) {
+    dispatch({
+      type: "LOAD_MY_MOVIE_LIST"
+    });
+  
     fetch("/movies")
-    .then( (response) => {
-      return response.json();
-    }).then((movies) => {
-      dipsatch(myMovieListLoaded(movies))
-    })
-  }
-
-  // return function (dispatch) {
-  //   dispatch({
-  //     type: "LOAD_USERS"
-  //   });
-  //   fetch("/users")
-  //   .then( (response) => {
-  //     return response.json();
-  //   }).then((users) => {
-  //     dispatch(usersLoaded(users));
-  //   });
-  // };
+      .then( (response) => {
+        return response.json();
+      }).then((movies) => {
+        dispatch(myMovieListLoaded(movies));
+      });
+  };
 }
+// return function (dispatch) {
+//   dispatch({
+//     type: "LOAD_USERS"
+//   });
+//   fetch("/users")
+//   .then( (response) => {
+//     return response.json();
+//   }).then((users) => {
+//     dispatch(usersLoaded(users));
+//   });
+// };
+// }
 export function myMovieListLoaded(movies) {
   return {
     type: "MY_MOVIE_LIST_LOADED",
     value: movies
   };
 }
-export function loadSearch(searchTerm) {
-  return function(dispatch){
-    type: "LOAD_SEARCH",
-  };
-  
-  fetch("https://api.themoviedb.org/3/search/multi?query=searchTerm&api_key=3feb152dee8cdcf2c8fabece6498529a")
-  .then( (response) => {
-    return response.json();
-  }).then((movies) => {
-    dipsatch(searchLoaded(movies))
-  })
-}
 
+export function loadSearch(searchTerm) {
+  return function (dispatch) {
+    dispatch({
+      type: "LOAD_SEARCH"
+    });
+    fetch("https://api.themoviedb.org/3/search/multi?query=searchTerm&api_key=3feb152dee8cdcf2c8fabece6498529a")
+      .then( (response) => {
+        return response.json();
+      }).then((movies) => {
+        dispatch(searchLoaded(movies));
+      });
+  };
+}
 
 export function searchLoaded(movies) {
   return {
@@ -52,13 +53,23 @@ export function searchLoaded(movies) {
 }
 
 export function saveMyMovie(movie) {
-  return function(dispatch){
+  return function (dispatch) {
    
     fetch("/movies", {
-      method:"POST",
-      headers: {"Content-Type":"application/JSON"},
+      method: "POST",
+      headers: {"Content-Type": "application/JSON"},
       body: JSON.stringify.movie
-    }).then(() => dispatch(loadMyMoviesList()))
+    }).then(() => dispatch(loadMyMoviesList()));
   
-  }
+  };
+}
+
+export function removeMyMovie(id) {
+  return function (dispatch) {
+    fetch("/movies/" + id, {
+      method: "delete"
+    }).then(response =>
+      response.json().then(() => dispatch(loadMyMoviesList)));
+  };
+    
 }
