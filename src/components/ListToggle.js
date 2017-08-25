fd595cb8f496a1813646d62498ff03de1f0ad136
stackbaxter <stackbaxter@yahoo.com>
2017-08-25 12:38:18 -0500

    ?finxing

--- src/components/ListToggle.js
+++ src/components/ListToggle.js
@@ -4,7 +4,7 @@ class ListToggle extends Component {
   constructor(props) {
     super(props);
     // determine if there is a json server id which means it was saved
-    this.state = { toggled: !props.movie ? false :  Boolean(props.movie._id)};
+    this.state = { toggled: props.movie ? false :  Boolean(props.movie._id)};
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
