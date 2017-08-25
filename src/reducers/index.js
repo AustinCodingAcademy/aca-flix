6f97105ecbcb7c20271c64eb6761bfb52dad65d5
stackbaxter <stackbaxter@gmail.com>
2017-08-25 13:11:15 -0500

    wasn't returning state!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ function myMovieList(state = [], action) {
   if (action.type === "MY_MOVIE_LIST_LOADED") {
     return action.value;
   }
+  return state;
 }
 
 function searchResults(state = [], action) {
   if (action.type === "SEARCH_RESULTS_LOADED") {
     return action.value;
   }
+  return state;
 }
 
 // function loadMyMovieList(state = [], action) {
