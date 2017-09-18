0e7ffea3dc576c628037fdfb45f390100c517a64
Ericka Emery <ericka@elonandcompany.com>
2017-09-17 22:13:06 -0500

    components created

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navigation(props){
+    return (
+        <div id="navigation" className="Navigation">
+        <nav>
+          <ul>
+            <li>Browse</li>
+            <li>My List</li>
+            <li>Top Picks</li>
+            <li>Recent</li>
+          </ul>
+        </nav>
+      </div>
+    );
+}
+
+export default Navigation;
+
