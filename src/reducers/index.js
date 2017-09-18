b5375271e852404d4047bc8effe3a14656918412
Ericka Emery <ericka@elonandcompany.com>
2017-09-17 22:40:02 -0500

    reducer values

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import {combineReducers} from "redux";
 
 function searchResults(state = [], action){
+    if (action.type === "MY_MOVIE_LIST_LOADED"){
+        return action.value;
+    }
     return state;
 }
 
 function myMovieList(state  = [], action){
+    if (action.type === "SEARCH_RESULTS_LOADED"){
+        return action.value;
+    }
     return state;
 }
 
