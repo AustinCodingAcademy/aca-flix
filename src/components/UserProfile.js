eb8efa24988eb9add663378d5e0a67e99744f062
Mac User <macuser@Macs-MacBook-Pro.local>
2017-11-06 18:08:13 -0800

    updated profile img src with valid link

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -6,7 +6,7 @@ function UserProfile() {
       <div className="User">
         <div className="name">Jack Oliver</div>
         <div className="image">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
+          <img src="http://images.all-free-download.com/images/graphiclarge/tiger_avatar_02_hd_pictures_169018.jpg" alt="profile" />
         </div>
       </div>
     </div>
@@ -15,3 +15,6 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
+// Previous Profile Image Link
+// <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
