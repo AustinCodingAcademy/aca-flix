e100a6175082de7b2683e6b9aeb07a0b4dab08fc
Jon Woo <jw56578@gmail.com>
2017-11-06 20:04:45 -0600

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import React from "react";
 import ListToggle from "./ListToggle";
 
 function Item(props) {
-  debugger;
+  
   let name = "";
   let backDrop = "http://image.tmdb.org/t/p/original" + props.movie.backdrop_path;
   if(!props.movie.name) {
