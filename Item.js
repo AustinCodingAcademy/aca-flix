b25944c8144c633127d55f5c720cecedd450fc46
Jesse-Paulk <paulkjesse@gmail.com>
2017-11-13 15:51:30 -0600

    fixed?

---
+++ Item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ListToggleContainer from '../containers/ListToggleContainer';
+
+function Item(props) {
+  let name = '';
+  let backDrop =
+    'http://image.tmdb.org/t/p/original' + props.movie.backdrop_path;
+  if (!props.movie.name) {
+    name = props.movie.original_title;
+  } else {
+    name = props.movie.name;
+  }
+
+  return (
+    <div className="Item" style={{ backgroundImage: 'url(' + backDrop + ')' }}>
+      <div className="overlay">
+        <div className="title">{name}</div>
+        <div className="rating">{props.movie.vote_average} / 10</div>
+        <div className="plot">{props.movie.overview}</div>
+        <ListToggleContainer movie={props.movie} />
+      </div>
+    </div>
+  );
+}
+export default Item;
