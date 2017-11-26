ca693e1e8045fbcb2262f0847d4f4b824ade72d8
jmwhitman7 <jonathanmwhitman@gmail.com>
2017-11-26 17:10:28 -0600

    checkpoint 1 complete

--- src/components/ListToggle.js
+++ src/components/ListToggle.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class ListToggle extends Component {
   constructor(props) {
-    
+
     super(props);
     // determine if there is a json server id which means it was saved
     this.state = { toggled: props.movie ? Boolean(props.movie._id) : false};
@@ -14,14 +14,14 @@ class ListToggle extends Component {
       this.setState({ toggled: false });
     } else {
       this.props.saveMyMovie(this.props.movie);
-      this.setState({ toggled: true }); 
+      this.setState({ toggled: true });
     }
   }
-  
+
   render() {
     return (
-      <div onClick={this.handleClick} 
-        data-toggled={this.state.toggled} 
+      <div onClick={this.handleClick}
+        data-toggled={this.state.toggled}
         className="ListToggle">
         <div>
           <i className="fa fa-fw fa-plus" />
