6aad96109671ae57dc69a42ec360a69f2f73852a
CruzMendez <camendez@utexas.edu>
2017-11-28 22:18:17 -0600

    initiation
    created: components, reducers, state/store.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from "./Logo.js";
 import TitleList from "./components/TitleList";
 import Hero from "./components/Hero";
 import SearchBox from "./components/SearchBox";
+import Navigation from "./components/Navigation";
+import UserProfile from "./components/UserProfile";
 
 class App extends Component {
   render() {
@@ -12,29 +14,13 @@ class App extends Component {
       <div>
         <header className="Header">
           <Logo />
-          {/*  <Navigation>   */}
-          <div id="navigation" className="Navigation">
-            <nav>
-              <ul>
-                <li>Browse</li>
-                <li>My list</li>
-                <li>Top picks</li>
-                <li>Recent</li>
-              </ul>
-            </nav>
-          </div>
-          {/*  </Navigation>   */}
+          
+          <Navigation />
+          
           <SearchBox />
-          {/*  <UserProfile>   */}
-          <div className="UserProfile">
-            <div className="User">
-              <div className="name">Jack Oliver</div>
-              <div className="image">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
-              </div>
-            </div>
-          </div>
-          {/*  </UserProfile>   */}
+          
+          <UserProfile />
+
         </header>
         <Hero />
         <TitleList 
