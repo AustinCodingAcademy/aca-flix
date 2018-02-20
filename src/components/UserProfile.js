a340817ac8b5dfb4c752f1b86adc6b2141a83696
Aaron Austin <aaronaustin@Aarons-MacBook-Pro.local>
2018-02-19 19:39:29 -0600

    commit 1 for advanced-checkpoint-1 Homework5

---
+++ src/components/UserProfile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navigation() {
+  
+
+  return (
+        <div className="UserProfile">
+            <div className="User">
+              <div className="name">Aaron Austin</div>
+              <div className="image">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
+              </div>
+            </div>
+          </div>
+  );
+}
+export default Navigation;
+
+
+
