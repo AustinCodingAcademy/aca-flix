0cfcb0713f8ea6e3803465ed778d166f634e4f0d
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-06-13 10:27:54 -0500

    added all necessary actions

---
+++ src/actions/index.js
@@ -0,0 +1,66 @@
+export function loadMyMovieList() {
+  return function (dispatch) {
+        
+    fetch("http://localhost:4000/movies")
+        .then( (response) => {
+          return response.json();
+        }).then((movies) => {
+          dispatch(myMovieListLoaded(movies));
+        });
+  
+  };
+}
+  
+export function myMovieListLoaded(movies) {
+  return {
+    type: "MY_MOVIE_LIST_LOADED",
+    value: movies
+  };
+}
+
+export function loadSearch(searchTerm) {
+    return function (dispatch) {
+          
+      fetch(`https://api.themoviedb.org/3/search/multi?query=${searchTerm}&api_key=b8c947674156a517c10f28a87cf7c0d9`)
+          .then( (response) => {
+            return response.json();
+          }).then((movies) => {
+            dispatch(searchLoaded(movies));
+          });
+    
+    };
+  }
+    
+  export function searchLoaded(movies) {
+    return {
+      type: "SEARCH_RESULTS_LOADED",
+      value: movies.results
+    };
+  }
+  
+  export function saveMyMovie() {
+    return function (dispatch) {
+          
+      fetch("http://localhost:4000/movies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+      }
+   )
+          .then( (response) => {
+            return response.json();
+          }).then((movies) => {
+            dispatch(loadMyMovieList(movies));
+          });
+    
+    };
+  }
+    
+  export function myMovieListLoaded(movies) {
+    return {
+      type: "MY_MOVIE_LIST_LOADED",
+      value: movies
+    };
+  }
