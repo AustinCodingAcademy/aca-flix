30b9848de706116ad42a6edf5777c900c458f161
mimitch2 <mike@melodix.com>
2018-06-13 08:19:05 -0500

    actions done for now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Logo from "./Logo.js";
 import TitleList from "./components/TitleList";
 import Hero from "./components/Hero";
+import Navigation from "./components/Navigation";
+import UserProfile from "./components/UserProfile";
 import SearchBox from "./components/SearchBox";
 
 class App extends Component {
@@ -12,29 +14,29 @@ class App extends Component {
       <div>
         <header className="Header">
           <Logo />
-          {/*  <Navigation>   */}
+          {/* <Navigation>   */}
           <div id="navigation" className="Navigation">
             <nav>
               <ul>
-                <li>Browse</li>
-                <li>My list</li>
-                <li>Top picks</li>
-                <li>Recent</li>
-              </ul>
+                  <li>Browse</li>
+                  <li>My list</li>
+                  <li>Top picks</li>
+                  <li>Recent</li>
+                </ul>
             </nav>
           </div>
-          {/*  </Navigation>   */}
+          {/* </Navigation>   */}
           <SearchBox />
-          {/*  <UserProfile>   */}
+          {/* <UserProfile>   */}
           <div className="UserProfile">
             <div className="User">
               <div className="name">Jack Oliver</div>
               <div className="image">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
-              </div>
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
+                </div>
             </div>
           </div>
-          {/*  </UserProfile>   */}
+          {/* </UserProfile>   */}
         </header>
         <Hero />
         <TitleList 
---
+++ src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = (props) => {
+  return; 
+};
+ 
+export default Navigation;
