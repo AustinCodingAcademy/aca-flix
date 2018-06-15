c9ec1c45bcf255e8ea00847e00d127c2517d0929
walzer85 <walzer85@gmail.com>
2018-06-15 18:52:50 -0500

    made it to the containers

---
+++ src/actions/index.js
@@ -0,0 +1,63 @@
+import { combineReducers } from "redux";
+
+export function loadMyMovieList(){
+    return function (dispatch) {
+        dispatch({
+            type: "LOAD_MOVIES"
+        });
+
+        fetch("/movies")
+        .then( (response) => {
+            return response.json();
+        }).then((movies) => {
+            dispatch(myMoviesLoaded(movies));
+        });
+    };
+};  
+
+export function loadMyMovieList(movies){
+    return {
+        type: "MY_MOVIE_LIST_LOADED",
+        movies: movies
+    }
+};
+
+export function loadSearch(searchTerm){
+    return function (dispatch) {
+        dispatch({
+            type: "LOAD_SEARCH"
+        });
+
+        fetch("https://api.themoviedb.org/3/search/multi?query=searchTerm&api_key=0dc94f449f50d8c8598f3f801426f127")
+        .then( (response) => {
+            return response.json();
+        }).then( (movies) => {
+            dispatch(searchLoaded(movies));
+        });
+    };
+};
+
+export function searchLoaded(movies){
+    return {
+        type: "SEARCH_RESULTS_LOADED",
+        movies: movies.results
+    }
+}
+
+export function saveMyMovie(movie){
+  return function (dispatch){
+    fetch("/movies", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(movie)
+    }).then(() => dispatch(loadMyMovieList()));
+  };
+}
+
+export function removeMyMovie(id){
+    fetch("/movies/${_id})", {
+        method: "DELETE"
+    }).then( (movies) => {
+        dispatch(loadMyMovieList(movies));
+    })
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+
+function myMovieList(state=[], action){
+    if(action.type==="MY_MOVIE_LIST_LOADED"){
+        return state;
+    }
+}
+
+function searchResults(state=[], action){
+    if(action.type==="SEARCH_RESULTS_LOADED"){
+        return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    myMovieList, searchResults
+})
+  
+export default rootReducer;
