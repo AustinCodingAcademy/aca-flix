d0d5318e5c2c0e5c219959696ef0106713170765
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-15 12:38:15 -0500

    fixing small things

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 //  MOVIELISTMOVIELISTMOVIELISTMOVIELISTMOVIELISTMOVIELISTMOVIELISTMOVIELIST
 export function loadMyMovieList() {
   return function (dispatch) {
-    // type: "LOAD_MY_MOVIE_LIST" (readme wanted this?),
-    fetch("http://localhost:5100/movies")
+    dispatch({
+      type: "LOAD_MY_MOVIE_LIST"
+    });
+    fetch("/movies")
       .then((response) => {
         return response.json();
       }).then((movies) => {
@@ -21,11 +23,12 @@ export function myMovieListLoaded(movies) {
 
 
 //  LOADSEARCHLOADSEARCHLOADSEARCHLOADSEARCHLOADSEARCHLOADSEARCHLOADSEARCHLOAD
-export function loadSearch() {
-    //  type: "LOAD SEARCH"
+export function loadSearch(searchTerm) {
   return function (dispatch) {
-      // type: "LOAD_MY_MOVIE_LIST" (readme wanted this?),
-    fetch("https://api.themoviedb.org/3/search/multi?query=searchTerm&api_key=4e7e4ac8e31254eb6c0a9531f3093321")
+    dispatch({
+      type: "LOAD_SEARCH"
+    });
+    fetch("https://api.themoviedb.org/3/search/multi?query=${searchTerm}&api_key=4e7e4ac8e31254eb6c0a9531f3093321")
     .then((response) => {
       return response.json();
     }).then((movies) => {
@@ -46,16 +49,16 @@ export function searchLoaded(movies) {
 //  SAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIESAVEMOVIE
 export function saveMyMovie(movie) {
   return function (dispatch) {
-    fetch("http://localhost:5100/movies", {
-      method: "post", 
+    fetch("/movies", {
+      method: "", 
       body: JSON.stringify(movie),
       headers: {
         "Content-Type": "application/json"
       }
     }).then(function (res) {
       return res.json();
-    }).then(function (m) {
-      dispatch(loadMyMovieList(m));
+    }).then(function () {
+      dispatch(loadMyMovieList());
     });
   };
 }
@@ -63,18 +66,14 @@ export function saveMyMovie(movie) {
 
 //  DELETEMOVIESDELETEMOVIESDELETEMOVIESDELETEMOVIESDELETEMOVIESDELETEMOVIESDELETEMOVIES
 //  not sure if this is right
-export function removeMyMovie(movie) {
+export function removeMyMovie(id) {
   return function (dispatch) {
-    fetch("http://localhost:5100/movies/id", {
-      method: "delete", 
-      body: JSON.stringify(movie),
-      headers: {
-        "Content-Type": "application/json"
-      }
+    fetch("/movies" + id, {
+      method: "DELETE", 
     }).then(function (res) {
       return res.json();
-    }).then(function (m) {
-      dispatch(loadMyMovieList(m));
+    }).then(function () {
+      dispatch(loadMyMovieList());
     });
   };
 }
