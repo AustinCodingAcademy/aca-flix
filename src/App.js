53c85f883fb615c23d4f2809ff060ceb5f9823d5
Matt Cameron <cameron.matt@gmail.com>
2018-06-16 10:29:59 -0500

    Navigation and UserProfile components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./App.css";
 import Logo from "./Logo.js";
 import TitleList from "./components/TitleList";
 import Hero from "./components/Hero";
 import SearchBox from "./components/SearchBox";
+import Naviation from "./components/Navigation";
+import UserProfile from "./components/UserProfile";
 
 class App extends Component {
   render() {
@@ -12,37 +14,13 @@ class App extends Component {
       <div>
         <header className="Header">
           <Logo />
-          {/*  <Navigation>   */}
-          <div id="navigation" className="Navigation">
-            <nav>
-              <ul>
-                <li>Browse</li>
-                <li>My list</li>
-                <li>Top picks</li>
-                <li>Recent</li>
-              </ul>
-            </nav>
-          </div>
-          {/*  </Navigation>   */}
+          <Naviation />
           <SearchBox />
-          {/*  <UserProfile>   */}
-          <div className="UserProfile">
-            <div className="User">
-              <div className="name">Jack Oliver</div>
-              <div className="image">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" />
-              </div>
-            </div>
-          </div>
-          {/*  </UserProfile>   */}
+          <UserProfile />
         </header>
         <Hero />
-        <TitleList 
-          title="Search Results" 
-          movies={this.props.searchResults} />
-        <TitleList 
-          title="My Movies" 
-          movies={this.props.myMovieList} />
+        <TitleList title="Search Results" movies={this.props.searchResults} />
+        <TitleList title="My Movies" movies={this.props.myMovieList} />
       </div>
     );
   }
