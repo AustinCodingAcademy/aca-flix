6b2d0b7c74426bad6319c0b887d5097693f55093
Matt Cameron <cameron.matt@gmail.com>
2018-06-16 10:35:30 -0500

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,16 @@
 import { combineReducers } from "redux";
 
 function searchResults(state = [], action) {
-  // if (action.type === "CONTACTS_LOADED") {
-  //   return action.value;
-  // }
-  // if (action.type === "CONTACT_CREATED") {
-  //   return [...state, action.value];
-  // }
+  if (action.type === "SEARCH_RESULTS_LOADED") {
+    return action.value;
+  }
   return state;
 }
 
 function myMovieList(state = [], action) {
-  // if (action.type === "CONTACTS_LOADED") {
-  //   return action.value;
-  // }
-  // if (action.type === "CONTACT_CREATED") {
-  //   return [...state, action.value];
-  // }
+  if (action.type === "MY_MOVIE_LIST_LOADED") {
+    return action.value;
+  }
   return state;
 }
 
