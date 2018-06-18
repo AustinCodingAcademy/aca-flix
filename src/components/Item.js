b8125906da1e6fac2f77988b80a862731bee4e22
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-17 21:08:01 -0500

    small changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ListToggleContainer from "./ListToggleContainer";
+import ListToggleContainer from "../containers/ListToggleContainer";
 
 function Item(props) {
   
   let name = "";
-  let backDrop = "http://image.tmdb.org/t/p/original" + props.movie.backdrop_path;
-  if(!props.movie.name) {
+  const backDrop = "http://image.tmdb.org/t/p/original" + props.movie.backdrop_path;
+  if (!props.movie.name) {
     name = props.movie.original_title;
   } else {
     name = props.movie.name;
   }
 
   return (
-    <div className="Item" style={{backgroundImage: 'url(' + backDrop + ')'}} >
+    <div className="Item" style={{backgroundImage: "url(' + backDrop + ')"}} >
       <div className="overlay">
         <div className="title">{name}</div>
         <div className="rating">{props.movie.vote_average} / 10</div>
