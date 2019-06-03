bdf25ba9c0d17b7a0d15e58eddaf60505605f893
Michael Snowden <mtsnowden@gmail.com>
2019-06-03 08:37:15 -0500

    working on actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,18 @@
-export function myMovieListLoaded(movies) {
+export const loadMyMovieList = () => {
+    return (dispatch) => {
+        fetch("./movies")
+        .then((res) => {
+            return res.json()
+        })
+        .then((movies) => {
+            dispatch(myMovieListLoaded(movies))
+        })
+    };
+}
 
+export const myMovieListLoaded = (movies) => {
+    return {
+    type: 'MY_MOVIE_LIST_LOADED',
+    value: movies
+    };
 }
